283f3028c064554f1cdbd843b9cb4ae45a2c2400
muirisc <91299876+muirisc@users.noreply.github.com>
2022-04-08 00:25:41 -0400

    New Skills Bar added

--- muirisc/src/About.js
+++ muirisc/src/About.js
@@ -23,12 +23,29 @@ const About = () => {
           <div>
             <p>Medium</p>
             </div>
-
     </div>
-
 </div>
+  </div>
+    <div className="skills">
+    <h3> Skills</h3>
+    <div className="skillList">
+    
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Ruby</li>
+        <li>Python</li>
+        <li>Photoshop</li>
+        <li>SQL</li>
+        <li>TypeScript</li>
+        <li>React.js</li>
+        <li>Ruby on Rails</li>
+        {/* <li>GitHub</li>
+        <li></li>
+        <li>Writing</li> */}
+      
       </div>
-
+    </div>
 
     </section>
   )
